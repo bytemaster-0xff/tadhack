6e46a764f1d3e1276121f10b7808ea3d66141b22
Kelvin McDaniel <kelvin.mcdaniel@software-logistics.com>
2022-10-15 18:30:26 -0400

    got floodzone document inserts working.

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,10 +2,10 @@ var express = require('express');
 var router = express.Router();
 const { MongoClient } = require('mongodb');
 const url = 'mongodb://localhost:27017';
-const client = new MongoClient(url);
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
+  const client = new MongoClient(url);
   await client.connect();
   console.log('Connected successfully to server - inserting document');
   const db = client.db("playground");
@@ -18,12 +18,14 @@ router.get('/', async function (req, res, next) {
 });
 
 router.get('/dataload/floodzones', async function (req, res, next) {
+  const client = new MongoClient(url);
   await client.connect();
   console.log('Connected successfully to server - inserting zone document');
   const db = client.db("playground");
   const collection = db.collection('documents');
 
   const documents = [
+    { 'phone': '3525592550', 'name': 'Dummy: 1', 'county': 'Pinellas', 'city': 'Palm Harbor', 'state': 'FL', 'zone': 'D' },
     { 'phone': '3525592559', 'name': 'GV: James', 'county': 'Pinellas', 'city': 'Palm Harbor', 'state': 'FL', 'zone': 'D' },
     { 'phone': '8134443963', 'name': 'GV: Kelvin', 'county': 'Hillsborough', 'city': 'Tampa', 'state': 'FL', 'zone': 'A' },
     { 'phone': '8502125744', 'name': 'Kelvin', 'county': 'Hillsborough', 'city': 'Tampa', 'state': 'FL', 'zone': 'A' },
@@ -35,19 +37,41 @@ router.get('/dataload/floodzones', async function (req, res, next) {
   ];
 
   let insertedCount = 0;
-  documents.forEach(async (document, i) => {
-    try {
-      await collection.insertOne(document);
-      insertedCount++; 
-    } catch (error) {
-      console.log('ERROR! Could not insert document:', document);
+  let promisesToKeep = [];
+  documents.forEach((document, i) => {
+    console.log('+++++++ document to insert', document);
+    if (document) {
+      promisesToKeep.push(collection.insertOne(document));
     }
   });
-
-  console.log(`OPERATION STATUS:`, `inserted ${insertedCount} document${insertedCount === 1 ? '' : 's'}`);
   
-  res.render('index', { title: 'IVR - Portal' });
-  client.close();
+  let responseMessage = 'failed!';
+
+  if (promisesToKeep.length > 0) {
+    console.log('+++++++ there are promises to keep', new Date());
+    try {
+      await Promise.all(promisesToKeep)
+      .then((responses, r) => {
+        console.log(`responses`, responses);
+      })
+      .finally(() => {
+        responseMessage = `Inserted ${insertedCount} document${insertedCount === 1 ? '' : 's'}`;
+        console.log(`OPERATION STATUS:`, responseMessage);
+      });
+      insertedCount++;
+    } catch (error) {
+      console.log('ERROR INSERTING DOCUMENTS!', new Date());
+    }
+    finally{
+      res.json({ message: responseMessage });
+      client.close(); 
+    }
+  }
+  else {
+    console.log(`OPERATION STATUS:`, responseMessage);
+    res.json({ message: responseMessage });
+    client.close(); 
+  }
 });
 
 module.exports = router;
