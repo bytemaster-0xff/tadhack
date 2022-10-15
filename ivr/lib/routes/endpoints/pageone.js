0b09eecea1893b9b4691af5fcda5c70f139fff57
Yoruyoru <33980541+JoshM-Yoru@users.noreply.github.com>
2022-10-15 14:52:23 -0400

    Add files via upload

--- ivr/lib/routes/endpoints/pageone.js
+++ ivr/lib/routes/endpoints/pageone.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const WebhookResponse = require('@jambonz/node-client').WebhookResponse;
-const text = `<speak>Hello, you have come to page one. This is a test.</speak>`;
+const text = `<speak>Hello, I will be assisting you in setting up a friends and family circle of who to contact, I will need a first name, last name, and their ten digit telephone number.</speak>`;
 
 router.post('/', (req, res) => {
   const {logger} = req.app.locals;
@@ -16,5 +16,63 @@ router.post('/', (req, res) => {
     res.sendStatus(503);
   }
 });
+
+router.post('/process', (req, res) => {
+    const {logger} = req.app.locals;
+    logger.debug({payload: req.body}, 'POST /pageone/process');
+    
+    logger.debug({number: req.body.digits});
+
+		try {
+				let first_name = new WebhookResponse();
+				let last_name = new WebhookResponse();
+				let phone_num = new WebhookResponse();
+
+				first_name.gather(
+						{
+								"say":{
+										"text": "Please say the person's first name."
+								}
+						},
+						{
+								"actionHook": '/pageone/collect/',
+								"input": ["speech"],
+								"finishOnKey":'#',
+						},
+				)
+
+				last_name.gather(
+						{
+								"say":{
+										"text": "Please say the person's last name."
+								}
+						},
+						{
+								"actionHook": '/pageone/collect/',
+								"input": ["speech"],
+								"finishOnKey":'#',
+						},
+				)
+
+				phone_num.gather(
+						{
+								"say":{
+										"text": "Please enter the person's ten digit telephone number."
+								}
+						},
+						{
+								"actionHook": '/pageone/collect/',
+								"input": ["digits", "speech"],
+								"numDigits": 10,
+								"finishOnKey":'#',
+						},
+				)
+
+		} catch (err) {
+
+		}
+
+
+  });
 
 module.exports = router;
