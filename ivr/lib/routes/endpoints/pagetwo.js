045a316daf917322881af6d50f43f230f1df1b34
GatorJAC <jacuomo@hotmail.com>
2022-10-16 10:26:22 -0400

    Error logging updated, added async

--- ivr/lib/routes/endpoints/pagetwo.js
+++ ivr/lib/routes/endpoints/pagetwo.js
@@ -1,12 +1,9 @@
 const router = require('express').Router();
 const WebhookResponse = require('@jambonz/node-client').WebhookResponse;
+const repo = require('./repo.js');
 
-const text = `<speak>Welcome to Page Two, so cool!</speak>`;
 
-router.post('/', (req, res) => {
-  const {logger} = req.app.locals;
-  logger.debug({payload: req.body}, 'POST /pagetwo');
-  console.log('>>>>>> PAGE 2 - POST ',  req.body);
+router.post('/', async (req, res) => {
   try {
     const app = new WebhookResponse();
     app
@@ -14,21 +11,16 @@ router.post('/', (req, res) => {
       .redirect({
         actionHook: "/pagetwo/firstname"
       });
-      //.pause({length: 1.5})
-      //.say({text});
     res.status(200).json(app);
   } catch (err) {
-    logger.error({err}, 'Error');
+    //logger.error({err}, 'Error');
     res.sendStatus(503);
   }
 });
 
-router.post('/firstname', (req, res) => {
-  const {logger} = req.app.locals;
-  logger.debug({payload: req.body}, 'POST /pagetwo/firstname');
+router.post('/firstname', async (req, res) => {
   console.log('>>>>>> PAGE 2 - FNAME ',  req.body);
   try {
-    const app = new WebhookResponse();
     app
     .gather({
         actionHook: '/pagetwo/lastname',
@@ -38,15 +30,18 @@ router.post('/firstname', (req, res) => {
     });
     res.status(200).json(app);
   } catch (err) {
-    logger.error({err}, 'Error');
+    //logger.error({err}, 'Error');
     res.sendStatus(503);
   }
 });
 
-router.post('/lastname', (req, res) => {
-  const {logger} = req.app.locals;
-  logger.debug({payload: req.body}, 'POST /pagetwo/lastname');
+router.post('/lastname', async (req, res) => {
   console.log('>>>>>> PAGE 2 - LNAME ',  req.body);
+  const app = new WebhookResponse();
+  var user = await repo.getUser(cleanPhoneNumber);
+  user.name = {'firstName': req.body.payload.transcript};
+  await repo.putUser(user);
+
   try {
     const app = new WebhookResponse();
     app
@@ -58,15 +53,16 @@ router.post('/lastname', (req, res) => {
     });
     res.status(200).json(app);
   } catch (err) {
-    logger.error({err}, 'Error');
+    //logger.error({err}, 'Error');
     res.sendStatus(503);
   }
 });
 
-router.post('/zipcode', (req, res) => {
-  const {logger} = req.app.locals;
-  logger.debug({payload: req.body}, 'POST /pagetwo/zipcode');
+router.post('/zipcode', async (req, res) => {
   console.log('>>>>>> PAGE 2 - ZIP ',  req.body);
+  var user = await repo.getUser(cleanPhoneNumber);
+  user.name.lastName = req.body.payload.transcript;
+  await repo.putUser(user);
   try{
     const app = new WebhookResponse();
     app
@@ -79,21 +75,21 @@ router.post('/zipcode', (req, res) => {
     });
   res.status(200).json(app);
   } catch (err) {
-    logger.error({err}, 'Error');
+    //logger.error({err}, 'Error');
     res.sendStatus(503);
   }
 });
 
-router.post('/done', (req, res) => {
-  const {logger} = req.app.locals;
+router.post('/done', async (req, res) => {
   console.log('>>>>>> PAGE 2 - DONE ',  req.body);
+  var user = await repo.getUser(cleanPhoneNumber);
+  user.name.zipcode = req.body.payload.digits;
+  await repo.putUser(user);
   try {
     const app = new WebhookResponse();
-    //app
-    //.say({text: `<speak>you pressed ${req.body.digits}</speak>`});
     res.status(200).json(app);
   } catch (err) {
-    logger.error({err}, 'Error');
+    //logger.error({err}, 'Error');
     res.sendStatus(503);
   }
 });
